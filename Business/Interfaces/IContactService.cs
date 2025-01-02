289bfe438e1c984780fe64fbb3acdb859b387500
Koderinjo <mebbon@protonmail.com>
2025-01-02 18:55:02 +0100

    Added Contact.cs nad IContactService.cs

---
+++ Business/Interfaces/IContactService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Business.Models;
+
+namespace Business.Interfaces
+{
+    public interface IContactService
+    {
+        List<Contact> GetAllContacts();
+        void AddContact(Contact contact);
+        void SaveContactsToFile(string filePath);
+        void LoadContactsFromFile(string filePath);
+    }
+}
---
+++ Business/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Business.Models
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string StreetAddress { get; set; }
+        public required string PostalCode { get; set; }
+        public required string City { get; set; }
+
+        public Contact()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
