ccafe260c3863a81b366293fcc75357cf0e5f770
Koderinjo <mebbon@protonmail.com>
2025-01-02 19:25:36 +0100

    Added ContactService.cs

---
+++ Business/Services/ContactService.cs
@@ -0,0 +1,48 @@
+﻿using Business.Interfaces;
+using Business.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Business.Services
+{
+    public class ContactService : IContactService
+    {
+        private List<Contact> _contacts;
+        private readonly string _filePath;
+
+        public ContactService(string filePath)
+        {
+            _contacts = [];
+            _filePath = filePath;
+            LoadContactsFromFile(_filePath);
+        }
+
+        public List<Contact> GetAllContacts()
+        {
+            return _contacts;
+        }
+
+        public void AddContact(Contact contact)
+        {
+            _contacts.Add(contact);
+            SaveContactsToFile(_filePath);
+        }
+
+        public void SaveContactsToFile(string filePath)
+        {
+            var json = JsonSerializer.Serialize(_contacts);
+            File.WriteAllText(filePath, json);
+        }
+
+        public void LoadContactsFromFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                var json = File.ReadAllText(filePath);
+                var contacts = JsonSerializer.Deserialize<List<Contact>>(json);
+                _contacts = contacts ?? new List<Contact>();
+            }
+        }
+    }
+}
