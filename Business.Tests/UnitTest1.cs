9c237b18d77526ae8b4f54d8f3bfe7ddcb059e68
Koderinjo <mebbon@protonmail.com>
2025-01-03 05:08:25 +0100

    Added one test

--- Business.Tests/UnitTest1.cs
+++ Business.Tests/UnitTest1.cs
@@ -1,11 +1,31 @@
-﻿namespace Business.Tests
+﻿using Business.Models;
+using Business.Services;
+using Xunit;
+
+namespace Business.Tests
 {
-    public class UnitTest1
+    public class ContactServiceTests
     {
         [Fact]
-        public void Test1()
+        public void AddContact_ShouldAddContactToList()
         {
+            string filePath = "test_contacts.json";
+            var contactService = new ContactService(filePath);
+            var contact = new Contact
+            {
+                FirstName = "Test",
+                LastName = "Testsson",
+                Email = "test@testsson.com",
+                PhoneNumber = "123-456-7890",
+                StreetAddress = "123 Test St",
+                PostalCode = "12345",
+                City = "Testville"
+            };
 
+            contactService.AddContact(contact);
+
+            var contacts = contactService.GetAllContacts();
+            Assert.Contains(contact, contacts);
         }
     }
 }
